7dde6be32dd974e49ada4129d1ca941dff54f72b
harshal <harshalpinge@gmail.com>
2026-04-09 10:01:37 +0530

    chore: remove env files from tracking

--- devmetrics-backend/scripts/debugApiKey.js
+++
@@ -1,9 +0,0 @@
-import "../bootstrap.js";
-import { query } from "../config/db.js";
-
-const result = await query(
-  "SELECT key, owner FROM api_keys LIMIT 1"
-);
-
-console.log("Your API Key:", result.rows[0]);
-process.exit(0);
